35c56362e0ac0cdf09d34ffc1d9c732547f42463
Subhra Bikiran Nayak <subhrabikiran@gmail.com>
2026-06-29 21:43:35 +0530

    commiting a signin/signup ui

---
+++ web/src/App.tsx
@@ -0,0 +1,10 @@
+import { SignupFormDemo } from "#components/features/auth/components/signup"
+const App = () => {
+  return (
+    <div>
+      <SignupFormDemo />
+    </div>
+  )
+}
+
+export default App
