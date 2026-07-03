9ce679a693124f2c2940dacc60d2b6cabcda52f6
Shreya4321-pandey <shreyap262004@gmail.com>
2026-07-03 17:54:27 +0530

    feat: Add albums feature with music selection option

--- web/src/components/features/feed/gallery.tsx
+++ web/src/components/features/feed/gallery.tsx
@@ -19,7 +19,7 @@ const gallery = () => {
   const links = [ 
     { title: "feed", icon: <IconPhoto className="h-full w-full text-neutral-500 dark:text-neutral-300" />, href: "/gallery" }, 
     { title: "people", icon: <IconUsersGroup className="h-full w-full text-neutral-500 dark:text-neutral-300" />, href: "/people" }, 
-    { title: "collections", icon: <IconLayoutDashboard className="h-full w-full text-neutral-500 dark:text-neutral-300" />, href: "/uc" }, 
+    { title: "collections", icon: <IconLayoutDashboard className="h-full w-full text-neutral-500 dark:text-neutral-300" />, href: "/albums" }, 
     { title: "upload", icon: <IconArrowNarrowUp className="h-full w-full text-neutral-500 dark:text-neutral-300" />, href: "/upload" }, 
     { title: "AI mode", icon: <IconTextScanAi className="h-full w-full text-neutral-500 dark:text-neutral-300" />, href: "/uc" }, 
     { title: "contribute", icon: <IconCode className="h-full w-full text-neutral-500 dark:text-neutral-300" />, href: "https://github.com/pxe-photos/pxephotos" }, 
@@ -29,7 +29,7 @@ const gallery = () => {
   return ( 
     <div className="relative min-h-screen w-full bg-transparent antialiased"> 
       <div className="absolute inset-0 z-0">
-        {/* <CanvasRevealEffectDemo3 hovered={true} />  */}
+        
       </div>
 
       {/* Adding overflow-hidden and bounding layout constraints stops line blowouts */}
